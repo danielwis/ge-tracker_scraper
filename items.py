ae0a2a7c2591a7ef2737bf9e50ae01bf3741fdc5
danielwis <daniel_williams@live.com>
2020-01-24 22:58:12 +0100

    Split script into funcs and vars separately

---
+++ items.py
@@ -0,0 +1,5 @@
+items = {
+	"Warrior Ring": "https://www.ge-tracker.com/item/warrior-ring",
+	"Mystic Mud Staff": "https://www.ge-tracker.com/item/mystic-mud-staff",
+	"Death Rune": "https://www.ge-tracker.com/item/death-rune",
+}
---
+++ sets.py
@@ -0,0 +1,15 @@
+ancientPageSet = {
+	"Ancient Page 1": "https://www.ge-tracker.com/item/ancient-page-1",
+	"Ancient Page 2": "https://www.ge-tracker.com/item/ancient-page-2",
+	"Ancient Page 3": "https://www.ge-tracker.com/item/ancient-page-3",
+	"Ancient Page 4": "https://www.ge-tracker.com/item/ancient-page-4",
+	"Set": "https://www.ge-tracker.com/item/book-of-darkness-page-set",
+}
+
+zamorakPageSet = {
+	"Zamorak Page 1": "https://www.ge-tracker.com/item/zamorak-page-1",
+	"Zamorak Page 2": "https://www.ge-tracker.com/item/zamorak-page-2",
+	"Zamorak Page 3": "https://www.ge-tracker.com/item/zamorak-page-3",
+	"Zamorak Page 4": "https://www.ge-tracker.com/item/zamorak-page-4",
+	"Set": "https://www.ge-tracker.com/item/unholy-book-page-set",
+}
